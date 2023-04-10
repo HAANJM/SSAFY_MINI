a63240b4d4234e0231c4a90269538169710cc268
HAAN <108118635+HAANJM@users.noreply.github.com>
2023-04-10 22:09:52 +0900

    230410
    A형 시험으로인해 user객체만 수정

--- SSAFY_MINI/src/com/ssafy/mini/vo/User.java
+++ SSAFY_MINI/src/com/ssafy/mini/vo/User.java
@@ -1,32 +1,136 @@
 package com.ssafy.mini.vo;
 
+import java.sql.Date;
+
 public class User {
 	
-	private String id;
-	private String password;
+	private int userNo; // DB PK
+	private String id; // 아이디
+	private String password; // 비밀번호
+	private String name; // 이름
+	private int age; // 나이
+	private String phone; // 전화번호(xxx-xxxx-xxxx 형태로 받기위해 string)
+	private String email; // 이메일
+	private String nickName; // 닉네임
+	private Date createDate; // 생성날짜
+	private Date updateDate; // 계정 최근 활동 날짜
+	private char status; // 회원 상태
+	
 	public User() {
 		super();
 	}
-	public User(String id, String password) {
+
+	public User(int userNo, String id, String password, String name, int age, String phone, String email,
+			String nickName, Date createDate, Date updateDate, char status) {
 		super();
+		this.userNo = userNo;
 		this.id = id;
 		this.password = password;
+		this.name = name;
+		this.age = age;
+		this.phone = phone;
+		this.email = email;
+		this.nickName = nickName;
+		this.createDate = createDate;
+		this.updateDate = updateDate;
+		this.status = status;
 	}
+
+	public int getUserNo() {
+		return userNo;
+	}
+
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	public char getStatus() {
+		return status;
+	}
+
+	public void setStatus(char status) {
+		this.status = status;
+	}
+
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", password=" + password + "]";
+		return "User [userNo=" + userNo + ", id=" + id + ", password=" + password + ", name=" + name + ", age=" + age
+				+ ", phone=" + phone + ", email=" + email + ", nickName=" + nickName + ", createDate=" + createDate
+				+ ", updateDate=" + updateDate + ", status=" + status + "]";
 	}
+	
+	
 	
 }
